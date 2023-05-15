19754883946e7af8b87abfe317a7e7d72becb3e4
khalipid1548 <khalipid1548@gmail.com>
2023-05-15 16:37:47 +0700

    success

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { Nunito } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
+import Navbar from './components/navbar/Navbar'
+import './globals.css'
+
+import LoginModal from './components/modals/LoginModal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
+
+export const metadata = {
+    title: 'Airbnb',
+    description: 'Airbnb Clone',
+}
+
+const font = Nunito({subsets: ['latin']})
+
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+
+    const currentUser = await getCurrentUser()
+
+    return (
+        <html lang='en'>
+            <body className={font.className}>
+                <ClientOnly>
+                    <ToasterProvider />
+                    <RegisterModal />
+                    <LoginModal/>
+                    <RentModal/>
+                    <SearchModal/>
+                    <Navbar currentUser={currentUser} />
+                </ClientOnly>
+
+                <div className='pb-20 pt-28 '>{children}</div>
+                
+            </body>
+        </html>
+    )
+}
---
+++ app/providers/ModalsProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import RegisterModal from "../components/modals/RegisterModal";
+
+
+
+const ModalsProvider = () => {
+  return ( 
+    <>
+     
+      <RegisterModal />
+     
+    </>
+   );
+}
+ 
+export default ModalsProvider;
